fa0a386c39726d25b554f3a883fddb3d145756ca
Tutorial-bot-guider <50870164+Tutorial-bot-guider@users.noreply.github.com>
2019-05-22 13:10:35 +0530

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -7,10 +7,10 @@ bot.on('message', message =>{
     // Variables
     var sender = message.author; //The person who sent the message
     var msg = message.content;
-    var prefix = ">" //The text before commands, you can put anything that you prefer
+    var prefix = "<" //The text before commands, you can put anything that you prefer
 
     if(message.author.id != "574534091203018762" && message.channel.id === "579229567819055104"){
-        if(msg.startsWith('>', 0)){
+        if(msg.startsWith('<', 0)){
             if(msg === prefix + "bang" && message.channel.id === "579229567819055104"){
                 message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
             }
