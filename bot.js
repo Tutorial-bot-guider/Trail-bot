b85a2bd1df0128760a041e6a3ec05c2c47bcafbd
Tutorial-bot-guider <50870164+Tutorial-bot-guider@users.noreply.github.com>
2019-06-20 18:06:17 +0530

    Update and rename Bot.js to bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = "-" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "574534091203018762" && message.channel.id === "579229567819055104"){
+    if(message.author.id != "574534091203018762" && message.channel.id === "589620018963021828"){
         if(msg.startsWith('-', 0)){
-            if(msg === prefix + "bang" && message.channel.id === "579229567819055104"){
+            if(msg === prefix + "bang" && message.channel.id === "589620018963021828"){
                 message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
             }
             else{
